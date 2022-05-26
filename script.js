3a988ec2e63f834efb217e26a1ada7673b6aa85c
Henry Frost <henry.90.frost@gmail.com>
2022-05-26 13:37:17 -0400

    working end game

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ let clickCounter = 0;
 let firstClick;
 let secondClick;
 let wait = false;
+const tiles = {};
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
 // it is based on an algorithm called Fisher Yates if you want ot research more
@@ -26,47 +27,59 @@ function shuffle(array) {
   // While there are elements in the array
   while (counter > 0) {
     // Pick a random index
+    
     let index = Math.floor(Math.random() * counter);
 
     // Decrease counter by 1
     counter--;
-
+    
     // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
+    //tiles[counter] = array[counter];
   }
 
   return array;
 }
 
 let shuffledColors = shuffle(COLORS);
+
 //this function checks to see if the game is over
 function isGameOver(){
-  const tiles = [];
-
-  
-  console.log(gameContainer);
-  // for(let i = 0; i<gameContainer.classList.length; i++){
-  //   tiles[i] = gameContainer.
-  // }
-  
+  let allTiles = false;
+  for(let num in tiles)
+  {
+    let tileBg = tiles[num].style.backgroundColor;
+    let tileColor = tiles[num].classList.value;
+    console.log(tileBg);
+    console.log(tileColor);
+    if(tileBg!==tileColor){
+      return(allTiles);
+      break;
+    }
+  }
+  allTiles=true;
+  return allTiles;
 }
 
 // this function loops over the array of colors
 // it creates a new div and gives it a class with the value of the color
 // it also adds an event listener for a click for each card
 function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
+  for (let color = 0; color < colorArray.length; color++) {
     // create a new div
     const newDiv = document.createElement("div");
-
+    
     // give it a class attribute for the value we are looping over
-    newDiv.classList.add(color);
+    newDiv.classList.add(colorArray[color]);
+    //console.log(color);
+    //console.log(newDiv);
 
     // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
-
+    newDiv.id = color;
+    tiles[color] = newDiv;
     // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
@@ -99,15 +112,19 @@ function handleCardClick(event) {
         clickCounter = 0;
       }
       else{
+        //Correct click
         wait=false;
         clickCounter = 0;
+        if(isGameOver()){
+          alert("You've won! Refresh the page to reload.");
+        };
       }
     }
   }
   else{
     console.log("YOU MUST WAIT!");
   }
-  isGameOver();
+  
 }
   
 
